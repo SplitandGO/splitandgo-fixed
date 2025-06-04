f527541884e5e9fdeeaa45d83aec850f08790f66
SplitandGO <splitandgo0@gmail.com>
2025-06-03 21:15:15 -0400

    Primer commit Split&Go

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '2rem' }}>
+      <h1>🍽️ Split&Go funcionando</h1>
+      <p>¡Tu app base está lista!</p>
+    </div>
+  );
+};
+
+export default App;
